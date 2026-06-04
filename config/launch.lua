8eeabcd8a9bdd3fd22c11f67c6c71987ef9e8565
basyicx <jerry.sy.bai@gmail.com>
2026-06-04 16:19:21 +0800

    forbidden fish in mac

--- config/launch.lua
+++ config/launch.lua
@@ -20,14 +20,14 @@ elseif platform.is_linux then
     options.default_prog = { 'bash', '-l' }
     options.launch_menu = {
         { label = 'Bash', args = { 'bash', '-l' } },
-        { label = 'Fish', args = { 'fish', '-l' } },
+        -- { label = 'Fish', args = { 'fish', '-l' } },
     }
 elseif platform.is_mac then
     options.default_prog = { 'zsh', '-l' }
     options.launch_menu = {
-        { label = 'Bash', args = { 'bash', '-l' } },
+        -- { label = 'Bash', args = { 'bash', '-l' } },
         { label = 'Zsh',  args = { 'zsh', '-l' } },
-        { label = 'Fish', args = { 'fish', '-l' } },
+        -- { label = 'Fish', args = { 'fish', '-l' } },
     }
 end
 
